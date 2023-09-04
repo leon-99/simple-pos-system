3118a26e3546bcd3a57fce48174f12073237022b
Win Khant Aung <leonzifer@gmail.com>
2023-09-04 18:51:14 +0630

    finished README.md

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,6 +19,7 @@
 
     @livewireStyles
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
@@ -25,7 +27,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -52,13 +56,14 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
@@ -77,9 +82,9 @@
         <main>
             <div class="d-flex">
                 <div class="col-md-2 position-fixed bg-dark text-center" style="height: 100vh;">
-                    <a href="{{route('index')}}" class="btn btn-sm btn-light my-3 w-75">Products List</a>
-                    <a href="{{route('dashboard')}}" class="btn btn-sm btn-light mb-3 w-75">Dashboard</a>
-                    <a href="{{route('create')}}" class="btn btn-sm btn-primary mb-3 w-75">New Sale</a>
+                    <a href="{{ route('index') }}" class="btn btn-sm btn-light my-3 w-75">Products List</a>
+                    <a href="{{ route('dashboard') }}" class="btn btn-sm btn-light mb-3 w-75">Dashboard</a>
+                    <a href="{{ route('create') }}" class="btn btn-sm btn-primary mb-3 w-75">New Sale</a>
 
                 </div>
                 <div class="col-md-10" style="margin-left: 225px">
@@ -88,6 +93,16 @@
             </div>
         </main>
     </div>
+
+    <footer class="text-center">
+        <p>
+            Copyright © 2023 Win Khant Aung. All rights reserved.
+        </p>
+        <p>
+            This website and its content are protected by copyright law and international treaties.
+        </p>
+    </footer>
     @livewireScripts
 </body>
+
 </html>
